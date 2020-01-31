12e7457a108de785ee7b04bf7a7c15972c993640
rlcjf0014 <joshuarule2010@gmail.com>
2020-01-31 17:23:17 +0900

    Downloaded essential modules

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+ import express, {Application, Request, Response, NextFunction} from 'express';
+
+ const app: Application = express();
+const port: number = 5000;
+
+
+ const add = (a: number, b: number): number => a+ b; 
+
+ app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    console.log(add(5, 5)) 
+    res.send('Hello');
+ });
+
+
+ app.listen(port, () => console.log(`Server Running on ${port}`));
