1512191ffac21a58fd1acb2177d7f0e6f3b4c6ea
melangyun <melangyun@gmail.com>
2020-02-02 13:08:17 +0900

    make Cat Entitiy

--- src/entity/Cat.ts
+++ src/entity/Cat.ts
@@ -1,19 +1,41 @@
 import {
-    Entity, BaseEntity, PrimaryGeneratedColumn, Column,
-  } from 'typeorm';
-  
-  @Entity()
-  export default class User extends BaseEntity {
-      @PrimaryGeneratedColumn()
-      id: number;
-  
-      @Column()
-      firstName: string;
-  
-      @Column()
-      lastName: string;
-  
-      @Column()
-      age: number;
-  }
-  
+  Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'cat' })
+class Cat extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({ type: 'nvarchar', comment: 'a brief explanation of about 200 characters based on uniCode.' })
+    description:string;
+
+    @Column({ type: 'point', nullable: 'false' })
+    location:string;
+
+    @Column({ type: 'varchar', nullable: 'false' })
+    nickname:string;
+
+    @Column({ type: 'varchar', nullable: 'false', length: 100 })
+    cut:string;
+
+    @Column({ type: 'varchar', nullable: 'false', length: 100 })
+    rainbow:string;
+
+    @Column({ type: 'varchar', nullable: 'true' })
+    species:string;
+
+    @Column({ type: 'varchar', nullable: 'true' })
+    today:string;
+
+    @Column({
+      type: 'timestamp', name: 'today_time', nullable: 'true', comment: 'Time when "today"column updated',
+    })
+    todayTime:Date;
+
+    @CreateDateColumn()
+    createAt: Date;
+
+    @UpdateDateColumn()
+    updateAt: Date;
+}
