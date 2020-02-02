39f582a5319d1f946063d0f7533f477864d6c56d
melangyun <melangyun@gmail.com>
2020-02-02 13:20:31 +0900

    add code after conflict resolution

--- src/data/entity/Cat.ts
+++ src/data/entity/Cat.ts
@@ -1,18 +1,41 @@
 import {
-  Entity, BaseEntity, PrimaryGeneratedColumn, Column,
+    Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn,
 } from "typeorm";
 
-@Entity()
-export default class Cat extends BaseEntity {
+@Entity({ name: "cat" })
+class Cat extends BaseEntity {
     @PrimaryGeneratedColumn()
-    id: number;
+    id:number;
 
-    @Column()
-    firstName: string;
+    @Column({ type: "nvarchar", comment: "a brief explanation of about 200 characters based on uniCode." })
+    description:string;
 
-    @Column()
-    lastName: string;
+    @Column({ type: "point", nullable: "false" })
+    location:string;
 
-    @Column()
-    age: number;
+    @Column({ type: "varchar", nullable: "false" })
+    nickname:string;
+
+    @Column({ type: "varchar", nullable: "false", length: 100 })
+    cut:string;
+
+    @Column({ type: "varchar", nullable: "false", length: 100 })
+    rainbow:string;
+
+    @Column({ type: "varchar", nullable: "true" })
+    species:string;
+
+    @Column({ type: "varchar", nullable: "true" })
+    today:string;
+
+    @Column({
+        type: "timestamp", name: "today_time", nullable: "true", comment: "Time when \"today\"column updated",
+    })
+    todayTime:Date;
+
+    @CreateDateColumn()
+    createAt: Date;
+
+    @UpdateDateColumn()
+    updateAt: Date;
 }
