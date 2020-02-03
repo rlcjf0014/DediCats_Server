ed133c9c9326e13cc68f1fc5570c0154af9b7f3c
rlcjf0014 <joshuarule2010@gmail.com>
2020-02-03 22:38:29 +0900

    map route

--- src/route/Map.ts
+++ src/route/Map.ts
@@ -3,10 +3,36 @@ import express from "express";
 
 const router:express.Router = express.Router();
 
-router.get("/", (req:express.Request, res:express.Response) => {
-    const reqData = req.body;
+router.post("/map", (req:express.Request, res:express.Response) => {
+    const {location} : {location:string} = req.body;
     // 타입 지정시  const defunt: PersoneModel = res.body; 형태로 사용
     console.log(`server received POST req from ip: ${req.ip}. data is ${reqData}`);
 });
+
+//* request.body.location
+//! "((33.44843745687413, 126.56798357402302), (33.452964008206735, 126.57333898904454))"
+
+// ? response
+/*
+[
+    {
+        cat_id:1,
+        cat_nickname: "돼냥이",
+        location:
+            [
+               latitude, longitude
+            ]
+    },
+    {
+        cat_id:2,
+        cat_nickname: "고라파덕",
+        location:
+            [
+               latitude, longitude
+            ]
+    }
+]
+*/
+
 
 export default router;
