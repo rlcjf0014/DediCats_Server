fb8e67927a51729e8dca0e4a14f4ffb481086af3
rlcjf0014 <joshuarule2010@gmail.com>
2020-02-04 14:23:44 +0900

    Edited tables

---
+++ src/data/entity/CatTag.ts
@@ -0,0 +1,39 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import {
+    Entity, BaseEntity,
+    PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne,
+} from "typeorm";
+
+
+import User from "./User";
+import Cat from "./Cat";
+import Tag from "./Tag";
+
+
+@Entity({name: "cat_tag"})
+export default class CatTag extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ type: "varchar", nullable: false })
+    status!: string;
+
+    @Column({ type: "varchar", nullable: false })
+    deleteUser!: string;
+
+    @CreateDateColumn({ name: "create_at" })
+    createAt! : Date;
+
+    @UpdateDateColumn({ name: "update_at" })
+    updateAt! : Date;
+
+    @ManyToOne((type) => User, (user) => user.catTags, { cascade: true, nullable: true })
+    user!: User;
+
+    @ManyToOne((type) => Cat, (cat) => cat.catTags, { cascade: true, nullable: true })
+    cat!: Cat;
+
+    @ManyToOne((type) => Tag, (tag) => tag.catTags, { cascade: true, nullable: true })
+    tag!: Tag;
+}
