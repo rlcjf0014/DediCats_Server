459ee4f3b21ae5f281ef6491e84d58fd62d42605
rlcjf0014 <joshuarule2010@gmail.com>
2020-02-06 17:13:19 +0900

    Working on cat route

--- src/route/Cat.ts
+++ src/route/Cat.ts
@@ -1,18 +1,22 @@
 import express from "express";
-import { getConnection, UpdateResult } from "typeorm";
+import {
+    getConnection, UpdateResult, InsertResult, getRepository,
+} from "typeorm";
 import CatTag from "../data/entity/CatTag";
 import Cat from "../data/entity/Cat";
+import Tag from "../data/entity/Tag";
+import Photo from "../data/entity/Photo";
 // import storage from "../data/storage";
 
 const router:express.Router = express.Router();
 
 // delete tag
 router.post("/deleteTag", async (req:express.Request, res:express.Response) => {
-    const { catTagId, userId }:{tagId:number, catTagId:number, userId:number } = req.body;
+    const { tagId, catId, userId }:{tagId:number, catId:number, userId:number } = req.body;
     try {
         const deleteTag:any = await getConnection().createQueryBuilder()
             .update(CatTag).set({ status: "D", deleteUser: userId })
-            .where("cat_tag.id= id", { id: catTagId })
+            .where({ tag: tagId, cat: catId })
             .execute();
         if (!deleteTag) {
             res.status(404).send("오류로 인해 태그 삭제가 실패했습니다");
@@ -47,10 +51,11 @@ router.post("/follow", async (req:express.Request, res:express.Response) => {
 });
 
 // This endpoint provides you with the information of the selected cat.
+// ? 캣 태그, 사진 같이 보내줘야 함.
 router.get("/:catId", async (req:express.Request, res:express.Response) => {
     const { catId }:{catId?: string} = req.params;
     try {
-        const getCat = await getConnection()
+        const getCat:any = await getConnection()
             .createQueryBuilder()
             .select("cat")
             .from(Cat, "cat")
@@ -60,7 +65,28 @@ router.get("/:catId", async (req:express.Request, res:express.Response) => {
             res.status(404).send("Cat not found");
             return;
         }
-        res.status(200).send(getCat);
+        const getTag:any = await getRepository(CatTag)
+            .createQueryBuilder("cat_tag")
+            .where({ cat: catId, status: "Y" })
+            .leftJoinAndSelect("cat_tag.tag", "tag")
+            .select(["cat_tag.id", "tag.content"])
+            .getMany();
+        if (!getTag) {
+            res.status(404).send("Cat not found");
+            return;
+        }
+        const getPhoto:any = await getConnection()
+            .createQueryBuilder()
+            .select("photo")
+            .from(Photo, "photo")
+            .where("photo.cat = :cat", { cat: catId, isProfile: "Y" })
+            .select(["photo.path"])
+            .getOne();
+        if (!getPhoto) {
+            res.status(404).send("Cat not found");
+            return;
+        }
+        res.status(200).send([getCat, getTag, getPhoto]);
     } catch (e) {
         res.status(409).send(e);
     }
@@ -78,13 +104,7 @@ router.post("/rainbow", async (req:express.Request, res:express.Response) => {
             .execute();
 
         if (updateCat.raw.changedRows) {
-            const returnObj:Cat|undefined = await getConnection()
-                .createQueryBuilder()
-                .select(["cat.id", "cat.rainbow"])
-                .from(Cat, "cat")
-                .where("cat.id = :id", { id: catId })
-                .getOne();
-            res.status(200).send(returnObj);
+            res.status(200).send(rainbow);
             return;
         }
         res.status(407).send("{'message': 'Could not update rainbow'}");
@@ -117,9 +137,23 @@ router.get("/follower/:catId", (req:express.Request, res:express.Response) => {
 });
 
 // Cat's Today Status
-router.post("/addcatTody", (req:express.Request, res:express.Response) => {
-    const { catId, catToday }:{catId?:number, catToday?:string} = req.body;
-
+router.post("/addcatToday", async (req:express.Request, res:express.Response) => {
+    const { catId, catToday }:{catId:number, catToday:string} = req.body;
+    try {
+        const now = Date();
+        const updateToday:any = await getConnection().createQueryBuilder()
+            .update(Cat).set({ today: catToday, todayTime: now })
+            .where("cat.id= :id", { id: catId })
+            .execute();
+        if (!updateToday) {
+            res.status(404).send("오늘의 상태 업데이트가 실패했습니다");
+            return;
+        }
+        const result = `{'cat_today': ${catToday}, 'cat_today_time': ${now}}`;
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(409).send(e);
+    }
     /*
     {
   "cat_today" : "기운이 넘침",
@@ -130,9 +164,24 @@ router.post("/addcatTody", (req:express.Request, res:express.Response) => {
 });
 
 // Catcut
-router.post("/cut", (req:express.Request, res:express.Response) => {
+router.post("/cut", async (req:express.Request, res:express.Response) => {
     const { catId, catCut }:{catId?:number, catCut?:object} = req.body;
-
+    try {
+        const updateCut:UpdateResult = await getConnection().createQueryBuilder()
+            .update(Cat).set({ cut: catCut })
+            .where("cat.id= :id", { id: catId })
+            .execute();
+        if (!updateCut) {
+            res.status(404).send("땅콩 업데이트가 실패했습니다");
+        }
+        if (updateCut.raw.changedRows) {
+            res.status(200).send(catCut);
+            return;
+        }
+        res.status(407).send("{'message': 'Could not update catcut'}");
+    } catch (e) {
+        res.status(409).send(e);
+    }
     /*
 { Y : 0 , N : 0 , unknown : 0}
 
@@ -140,8 +189,61 @@ router.post("/cut", (req:express.Request, res:express.Response) => {
 });
 
 // update Tag
-router.post("/updateTag", (req:express.Request, res:express.Response) => {
-    const { catId, catTag }:{catId?:number, catTag?:string} = req.body;
+router.post("/updateTag", async (req:express.Request, res:express.Response) => {
+    const { userId, catId, catTag }:{userId: number, catId:number, catTag:string} = req.body;
+    try {
+        const checkTag:any = await getConnection()
+            .createQueryBuilder()
+            .select("tag").from(Tag, "tag")
+            .where("tag.content = :content", { content: catTag })
+            .select(["tag.id"])
+            .getOne();
+        if (checkTag) {
+            const updateTag:any = await getConnection()
+                .createQueryBuilder()
+                .insert()
+                .into("cat_tag")
+                .values([{
+                    user: userId, cat: catId, tag: checkTag.id, status: "Y",
+                }])
+                .execute();
+            if (!updateTag) {
+                res.status(404).send("오류로 인해 태그 업데이트가 실패했습니다");
+            }
+            const result = `{"message": "Tag updated successfully", "catTag": ${catTag}}`;
+            res.status(200).send(result);
+        } else {
+            const newTag:InsertResult = await getConnection()
+                .createQueryBuilder()
+                .insert()
+                .into("tag")
+                .values([
+                    {
+                        content: catTag,
+                    },
+                ])
+                .execute();
+            if (!newTag) {
+                res.status(404).send("오류로 인해 태그 업데이트가 실패했습니다");
+                return;
+            }
+            const updateTag:any = await getConnection()
+                .createQueryBuilder()
+                .insert()
+                .into("cat_tag")
+                .values([{
+                    user: userId, cat: catId, tag: newTag.identifiers[0].id, status: "Y",
+                }])
+                .execute();
+            if (!updateTag) {
+                res.status(404).send("오류로 인해 태그 업데이트가 실패했습니다");
+            }
+            const result = `{"message": "Tag updated successfully", "catTag": ${catTag}}`;
+            res.status(200).send(result);
+        }
+    } catch (e) {
+        res.status(409).send(e);
+    }
 
     /*
 { "messge" : "Tag Add Successfully", "catTag": [뚱땡] }
