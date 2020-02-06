9afb237c240f40c5cae36e0ac2e8259c3fafd550
rlcjf0014 <joshuarule2010@gmail.com>
2020-02-06 14:30:07 +0900

    Moving on to next route

--- src/route/Map.ts
+++ src/route/Map.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import wkx from "wkx";
 // import storage from "../data/storage";
-
+//* "location": "POINT(1 2)",
+//* new wkx.Point(1, 2).toWkt();
 const router:express.Router = express.Router();
 
 router.post("/", (req:express.Request, res:express.Response) => {
     const {location} : {location:string} = req.body;
+    
+
     // 타입 지정시  const defunt: PersoneModel = res.body; 형태로 사용
     console.log(`server received POST req from ip: ${req.ip}. data is ${req.body}`);
 });
