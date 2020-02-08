6031099fd2c53cba508523f7c54e8588de23fc7b
melangyun <melangyun@gmail.com>
2020-02-09 00:03:26 +0900

    Modified map router to Google Map API

--- src/route/Map.ts
+++ src/route/Map.ts
@@ -6,30 +6,12 @@ import { getConnection } from "typeorm";
 const router:express.Router = express.Router();
 
 router.post("/", async (req:express.Request, res:express.Response):Promise<any> => {
-    const { location } : {location:string} = req.body;
-
-    const locationConverter:Function = (locationStr:string):Object => {
-        const trimLocation = locationStr.trim();
-        const startPoint:string = trimLocation.substring(2, trimLocation.indexOf(")"));
-        const endPoint:string = trimLocation.substring(trimLocation.lastIndexOf("(") + 1, trimLocation.lastIndexOf("))"));
-
-        const startLatitude:number = Number(startPoint.substring(0, startPoint.indexOf(",")));
-        const startLongitude:number = Number(startPoint.substring(startPoint.indexOf(",") + 1).trim());
-        const endLatitude:number = Number(endPoint.substring(0, endPoint.indexOf(",")));
-        const endLongitude:number = Number(endPoint.substring(endPoint.indexOf(",") + 1).trim());
-
-        return {
-            startLatitude, startLongitude, endLatitude, endLongitude,
-        };
-    };
-
-    const point:{startLatitude:number, startLongitude:number, endLatitude:number, endLongitude:number} = locationConverter(location);
-
+    const { location } : {location:{ NElatitude : number, NElongitude : number, SWlatitude : number, SWlongitude : number }} = req.body;
+    console.log(location);
     try {
         const result:Array<object> = await getConnection()
             .query("select * from (select id as `catId`, nickname as `catNickname` , X(`location`) as `latitude`, Y(`location`) As `longitude` from cat) as `innertable` where innertable.latitude >= ? and innertable.latitude <= ? and innertable.longitude >= ? and innertable.longitude <= ? ;",
-                [point.startLatitude, point.endLatitude, point.startLongitude, point.endLongitude]);
-
+                [location.NElatitude, location.SWlatitude, location.NElongitude, location.SWlongitude]);
         res.status(200).send(result);
     } catch (e) {
         // eslint-disable-next-line no-console
@@ -38,3 +20,22 @@ router.post("/", async (req:express.Request, res:express.Response):Promise<any> 
 });
 
 export default router;
+
+/*
+const locationConverter:Function = (locationStr:string):Object => {
+    const trimLocation = locationStr.trim();
+    const startPoint:string = trimLocation.substring(2, trimLocation.indexOf(")"));
+    const endPoint:string = trimLocation.substring(trimLocation.lastIndexOf("(") + 1, trimLocation.lastIndexOf("))"));
+
+    const startLatitude:number = Number(startPoint.substring(0, startPoint.indexOf(",")));
+    const startLongitude:number = Number(startPoint.substring(startPoint.indexOf(",") + 1).trim());
+    const endLatitude:number = Number(endPoint.substring(0, endPoint.indexOf(",")));
+    const endLongitude:number = Number(endPoint.substring(endPoint.indexOf(",") + 1).trim());
+
+    return {
+        startLatitude, startLongitude, endLatitude, endLongitude,
+    };
+};
+
+const point:{startLatitude:number, startLongitude:number, endLatitude:number, endLongitude:number} = locationConverter(location);
+*/
