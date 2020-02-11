1c1e151a38daff30fc57daf166bd815ceedc674a
melangyun <melangyun@gmail.com>
2020-02-12 01:36:50 +0900

    edit map router

--- src/route/Map.ts
+++ src/route/Map.ts
@@ -8,9 +8,9 @@ const router:express.Router = express.Router();
 router.post("/", async (req:express.Request, res:express.Response):Promise<any> => {
     const { location } : {location:{ NElatitude : number, NElongitude : number, SWlatitude : number, SWlongitude : number }} = req.body;
     try {
-        // ! description , follower , photo , address point 더 필요함
+        // ! follower , photo , address point 더 필요함
         const result:Array<object> = await getConnection()
-            .query("select * from (select id as `catId`, nickname as `catNickname` , X(`location`) as `latitude`, Y(`location`) As `longitude` from cat) as `innertable` where innertable.latitude >= ? and innertable.latitude <= ? and innertable.longitude >= ? and innertable.longitude <= ? ;",
+            .query("select * from (select id as `catId`, nickname as `catNickname` , X(`location`) as `latitude`, Y(`location`) as `longitude`, description as `description` from cat) as `innertable` where innertable.latitude >= ? and innertable.latitude <= ? and innertable.longitude >= ? and innertable.longitude <= ? ;",
                 [location.NElatitude, location.SWlatitude, location.NElongitude, location.SWlongitude]);
         res.status(200).send(result);
     } catch (e) {
