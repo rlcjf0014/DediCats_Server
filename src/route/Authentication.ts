f4ea6b91c5e48547579296a8ca67c87902cb3580
rlcjf0014 <joshuarule2010@gmail.com>
2020-02-11 14:40:18 +0900

    categorized authentication

---
+++ src/route/Authentication.ts
@@ -0,0 +1,79 @@
+import express, { NextFunction } from "express";
+import util from "util";
+import {
+    getConnection, InsertResult, UpdateResult,
+} from "typeorm";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import User from "../data/entity/User";
+
+require("dotenv").config();
+
+const router:express.Router = express.Router();
+
+
+router.post("/signin", async (req:express.Request, res:express.Response) => {
+    const { email, password }:{email:string, password:string} = req.body;
+    if (!email) {
+        res.status(409).send("email is required!");
+        return;
+    }
+    if (!password) {
+        res.status(409).send("password is required!");
+        return;
+    }
+    try {
+        const user:User|undefined = await getConnection()
+            .createQueryBuilder()
+            .select("user")
+            .from(User, "user")
+            .where("user.email = :email", { email })
+            .getOne();
+        // ! 유효하지 않은 이메일
+        if (!user) {
+            res.status(409).send("Invalid Email");
+            return;
+        }
+        // ? 암호화 후 비교
+        const pdkdf2Promise:Function = util.promisify(crypto.pbkdf2);
+        const key:Buffer = await pdkdf2Promise(password, user.salt, 105123, 64, "sha512");
+        const encryPassword:string = key.toString("base64");
+        if (encryPassword !== user.password) {
+            res.status(409).send("Incorrect password.");
+            return;
+        }
+        // ! 토큰 발급
+        const payload:{id:number, nickname:string, email:string} = {
+            id: user.id,
+            nickname: user.nickname,
+            email: user.email,
+        };
+        const accessKey:any = process.env.JWT_SECRET_ACCESS;
+        const options:{expiresIn:number} = { expiresIn: 60 * 60 * 24 };
+        const accessToken = jwt.sign(payload, accessKey, options);
+        res.json({ accessToken });
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
+});
+function authenticationToken (req:express.Request, res:express.Response, next:NextFunction) {
+    const authHeader = req.headers.authorization;
+    const token:any = authHeader && authHeader.split(" ")[1];
+    if (token === null) return res.sendStatus(401);
+    const accessKey:any = process.env.JWT_SECRET_ACCESS;
+    // eslint-disable-next-line consistent-return
+    jwt.verify(token, accessKey, (err:Error, user:any) => {
+        if (err) return res.send(403);
+        req.user = user;
+        next();
+    });
+}
+
+// ! 비밀번호 확인단계필요 ( 비밀번호 보안 필요함! )
+router.post("/signout", (req:express.Request, res:express.Response) => {
+    const { userId }:{userId:number} = req.body;
+    // response
+    // {"message": "Successfully signed out!"}
+});
+export default router;
