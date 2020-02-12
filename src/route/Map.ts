1c2a4b368437a97328d3809e3da01c6256710d1f
melangyun <melangyun@gmail.com>
2020-02-12 15:17:26 +0900

    edit map request

--- src/route/Map.ts
+++ src/route/Map.ts
@@ -1,17 +1,34 @@
 import express from "express";
 //* "location": "POINT(1 2)",
 //* new wkx.Point(1, 2).toWkt();
-import { getConnection } from "typeorm";
+import { getConnection, getRepository } from "typeorm";
+
+
+import Cat from "../data/entity/Cat";
+
 
 const router:express.Router = express.Router();
 
 router.post("/", async (req:express.Request, res:express.Response):Promise<any> => {
     const { location } : {location:{ NElatitude : number, NElongitude : number, SWlatitude : number, SWlongitude : number }} = req.body;
     try {
-        // ! follower , photo , address point 더 필요함
+        // ! follower ,  더 필요함
         const result:Array<object> = await getConnection()
-            .query("select * from (select id as `catId`, nickname as `catNickname` , X(`location`) as `latitude`, Y(`location`) as `longitude`, description as `description` from cat) as `innertable` where innertable.latitude >= ? and innertable.latitude <= ? and innertable.longitude >= ? and innertable.longitude <= ? ;",
+            .query("select innertable.catId, innertable.catNickname, innertable.catAddress, innertable.latitude, innertable.longitude, innertable.description, photo.path as `catProfile` from (select id as `catId`, nickname as `catNickname`, address as `catAddress`, X(`location`) as `latitude`, Y(`location`) as `longitude`, description as `description` from cat ) as `innertable` left join `photo` on(innertable.catId  = photo.catId and photo.is_profile = 'Y') where innertable.latitude >= ? and innertable.latitude <= ? and innertable.longitude >= ? and innertable.longitude <= ? ;",
                 [location.NElatitude, location.SWlatitude, location.NElongitude, location.SWlongitude]);
+
+        // for (let i = 0; i < result.length; i += 1) {
+        //     // eslint-disable-next-line no-await-in-loop
+        //     const getFollower:Array<object> = await getRepository(Cat).createQueryBuilder("cat")
+        //         .where("cat.id = :id", { id: result[i].catId })
+        //         .leftJoinAndSelect("cat.users", "user")
+        //         .select(["cat.id", "user.id", "user.nickname", "user.photoPath"])
+        //         .getMany();
+        //     if (!getFollower) {
+        //         res.status(409).send("Followers not found");
+        //     }
+
+        // }
         res.status(200).send(result);
     } catch (e) {
         // eslint-disable-next-line no-console
