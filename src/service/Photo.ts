65f7a23d96fd5439fb40b0d6cad5c39170381daa
melangyun <melangyun@gmail.com>
2020-02-21 20:28:57 +0900

    Edited updateProfile

--- src/service/Photo.ts
+++ src/service/Photo.ts
@@ -12,7 +12,7 @@ const addCatPhoto = (imagePath:string|boolean, catId:number):Promise<InsertResul
 const getCatPhoto = (catId:string):Promise<Photo|undefined> => PhotoDAO.getCatPhoto(catId);
 const getCatAlbum = (catId:string):Promise<Array<object>> => PhotoDAO.getCatAlbum(catId);
 const deleteProfile = (userId:number):Promise<UpdateResult> => PhotoDAO.deleteProfile(userId);
-const updateProfile = (userId:number, imagepath:string):Promise<UpdateResult> => PhotoDAO.updateProfile(userId, imagepath);
+const updateProfile = (userId:number, imagepath:string, photoName:string):Promise<UpdateResult> => PhotoDAO.updateProfile(userId, imagepath, photoName);
 const deletePostPhoto = (postId:number):Promise<UpdateResult> => PhotoDAO.deletePostPhoto(postId);
 const addPostPhoto = (imagepath:string, catId:number, postId:number):Promise<InsertResult> => PhotoDAO.addPostPhoto(imagepath, catId, postId);
 
