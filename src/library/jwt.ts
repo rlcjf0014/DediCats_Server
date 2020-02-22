fdd704ae685a8442c8d5bb18fbaaad64925b3590
rlcjf0014 <joshuarule2010@gmail.com>
2020-02-22 20:29:17 +0900

    Token edit

--- src/library/jwt.ts
+++ src/library/jwt.ts
@@ -1,9 +1,6 @@
 /* eslint-disable no-unused-vars */
 import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
 import { User } from "../model";
-import { UserService } from "../service";
-
 
 require("dotenv").config();
 
@@ -34,32 +31,6 @@ const generateAccessToken = (user:User):string => jwt.sign({
 
 const generateRefeshToken = (id:number):string => jwt.sign({ id }, refresKey, { expiresIn: "60s" });
 
-const reissueAccessToken = async (req:Request, res:Response, next:NextFunction) => {
-    const { refreshToken } = req.signedCookies;
-    let userId:number|undefined;
-    try {
-        userId = getUserIdbyRefreshToken(refreshToken);
-    } catch (e) {
-        next(e);
-    }
-    if (userId === undefined) return res.status(401).send("Invalid Request Token");
-    const user:User|undefined = await UserService.getUserById(userId);
-
-    if (!user?.refreshToken || user?.refreshToken !== refreshToken) return res.status(401).send("Invalid Request Token");
-    const accessToken:any = generateAccessToken(user);
-    res.cookie("accessToken", accessToken, { maxAge: 1000 * 60 * 60 * 24, signed: true });
-    const {
-        id, nickname, photoPath, createAt, email,
-    } = user;
-    return {
-        accessToken,
-        user: {
-            id, nickname, photoPath, createAt, email,
-        },
-    };
-};
-
-
 export {
-    getUserIdbyAccessToken, getUserIdbyRefreshToken, generateAccessToken, generateRefeshToken, reissueAccessToken,
+    getUserIdbyAccessToken, getUserIdbyRefreshToken, generateAccessToken, generateRefeshToken,
 };
